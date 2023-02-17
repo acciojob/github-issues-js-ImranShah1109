d7c4700b527f750eeb3ffaf7897135ee3291ea27
ImranShah1109 <imranshaha41@gmail.com>
2023-02-17 18:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ pageNumH3 = document.querySelector('span');
 nxtBtn = document.getElementById('load_next');
 prvBtn = document.getElementById('load_prev');
 
+let list;
+
 let pageNumber = 1;
 
 function fetchData(pageNumber) {
-	fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5.`)
+	fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`)
 		.then((response) =>response.json())
 		.then((data) => {
-			console.log(data);
+			// console.log(data.id);
+			list = document.createElement('ul');
+			for (let d of data) {
+				// console.log(d.id);
+                let li = document.createElement('li');
+                li.innerHTML = d.id;
+                list.appendChild(li);
+			}
+            document.body.appendChild(list);
 		})
 }
 
