c3e63e7ae104f94b92a7d3f83b9e9d8174d4aff9
ImranShah1109 <imranshaha41@gmail.com>
2023-02-17 17:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your code here
+pageNumH3 = document.querySelector('span');
+nxtBtn = document.getElementById('load_next');
+prvBtn = document.getElementById('load_prev');
+
+let pageNumber = 1;
+
+function fetchData(pageNumber) {
+	fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5.`)
+		.then((response) =>response.json())
+		.then((data) => {
+			console.log(data);
+		})
+}
+
+nxtBtn.addEventListener('click', () => {
+	pageNumber++;
+	if(pageNumber == 2){
+		prvBtn.disabled = false;
+	}
+	fetchData(pageNumber);
+	pageNumH3.innerText = pageNumber;
+});
+
+prvBtn.addEventListener('click', () => {
+	pageNumber--;
+	if(pageNumber <= 1){
+		prvBtn.disabled = true;
+	}
+	else{
+		prvBtn.disabled = false;
+	}
+	fetchData(pageNumber);
+	pageNumH3.innerText = pageNumber;
+});
+
+fetchData(1);
